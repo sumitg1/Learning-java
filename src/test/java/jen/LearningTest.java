f824610701fd41bc6b77e230dc74e394ccb4b052
Sumit Ghosh <sumit_4ever@rediffmail.com>
2016-06-13 20:42:12 +0530

    new Changes

--- src/test/java/jen/LearningTest.java
+++ src/test/java/jen/LearningTest.java
@@ -1,29 +1,22 @@
 package jen;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import DriverUtilities.WebDriverConfigure;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
-public class LearningTest {
-	private WebDriver driver;
+public class LearningTest extends WebDriverConfigure {
 
 	@Test
 	public void testEasy() {
-		driver.get("http://www.google.com");
-		String title = driver.getTitle();
-		Assert.assertTrue(title.contains("Google"));
-	}
 
-	@BeforeTest
-	public void beforeTest() {
-		driver = new FirefoxDriver();
+		getDriver().get("http://www.google.com");
+		String title = getDriver().getTitle();
+		Assert.assertTrue(title.contains("Google"));
 	}
 
 	@AfterTest
 	public void afterTest() {
-		driver.quit();
+		getDriver().quit();
 	}
 }
