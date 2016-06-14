0f1e972cff770fd28cedb4f02149d5cab94bd045
Sumit Ghosh <sumit_4ever@rediffmail.com>
2016-06-14 16:49:53 +0530

    g

--- src/test/java/jen/LearningTest.java
+++ src/test/java/jen/LearningTest.java
@@ -6,9 +6,10 @@ import org.testng.annotations.Test;
 public class LearningTest extends WebDriverConfigure {
 
 	@Test
-	public void testEasy() {
+	public void testEasy() throws InterruptedException {
 
 		driver.get("http://www.google.com");
+		Thread.sleep(5000);
 		Assert.assertTrue(driver.getTitle().contains("Google"));
 		
 	}
