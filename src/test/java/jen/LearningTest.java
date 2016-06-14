117af2fc97eff9cc9249456ee047762a8134d3f5
Sumit Ghosh <sumit_4ever@rediffmail.com>
2016-06-14 17:19:41 +0530

    new

--- src/test/java/jen/LearningTest.java
+++ src/test/java/jen/LearningTest.java
@@ -1,16 +1,17 @@
 package jen;
 
-import DriverUtilities.WebDriverConfigure;
+import test.test;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
-public class LearningTest extends WebDriverConfigure {
+public class LearningTest extends test {
 
 	@Test
 	public void testEasy() throws InterruptedException {
 
 		driver.get("http://www.google.com");
 		Thread.sleep(5000);
-		Assert.assertTrue(driver.getTitle().contains("Google"));
+		Assert.assertTrue(AssertTrueMethod("Google"));
 		
 	}
 
