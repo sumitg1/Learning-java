aac379359477c10cda9188e6e58127df6ace0ca5
Sumit Ghosh <sumit_4ever@rediffmail.com>
2016-06-14 16:40:27 +0530

    change

--- src/test/java/jen/LearningTest.java
+++ src/test/java/jen/LearningTest.java
@@ -3,20 +3,15 @@ package jen;
 import DriverUtilities.WebDriverConfigure;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.annotations.AfterTest;
-
 public class LearningTest extends WebDriverConfigure {
 
 	@Test
 	public void testEasy() {
 
-		getDriver().get("http://www.google.com");
-		String title = getDriver().getTitle();
-		Assert.assertTrue(title.contains("Google"));
+		driver.get("http://www.google.com");
+		Assert.assertTrue(driver.getTitle().contains("Google"));
+		
 	}
 
-	@AfterTest
-	public void afterTest() {
-		getDriver().quit();
-	}
+
 }
